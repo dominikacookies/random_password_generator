c9642f9f716b141bedd5f76e6af8dc6e648de017
Dominika <domi_nika@hotmail.co.uk>
2021-03-14 14:52:08 +0000

    added length verification logic

--- Develop/script.js
+++ Develop/script.js
@@ -19,30 +19,31 @@ function generatePassword() {
 	// Password length declaration by user
 	const lengthChoice = prompt("How long would you like your password to be?");
 	// Can't be shorter than 8  or more than 128
-
+	if (!lengthChoice) {
+    alert("Please enter the password length you would like.");
+	} else if ( lengthChoice > 128 || lengthChoice < 8) { 
+		alert("Please enter a password length that's more than 8 and less than 128 characters.");
+	} else {
 	// Criteria selection by user
-
 	// Uppercase letters
-  const uppercaseChoice = confirm("Would you like to include uppercase letters in your password?");
-
+	const uppercaseChoice = confirm("Would you like to include uppercase letters in your password?");
 	// Lowercase letters
-  const lowercaseChoice = confirm("Would you like to include lowercase letters in your password?");
-
+	const lowercaseChoice = confirm("Would you like to include lowercase letters in your password?");
 	// Numerics
-  const numericChoice = confirm("Would you like to include numbers in your password?");
-
+	const numericChoice = confirm("Would you like to include numbers in your password?");
 	// Special characters
-  const specialChoice = confirm("Would you like to include any special characters in your password?");
+	const specialChoice = confirm("Would you like to include any special characters in your password?");
+	// Has user selected at least one of the above?
+	}
 
 	// Has user selected at least one of the above?
 
 
 
   // sample special characters @%+\\/'!#$^?:,)(}{][~-_.
-	...
 
 	// Return our created password
-	return password;
+	//return password;
 }
 
 // Write password to the #password input
