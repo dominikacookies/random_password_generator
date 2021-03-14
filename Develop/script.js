6b38da2496afe97bce34ad2d2eff8c7470566a77
Dominika <domi_nika@hotmail.co.uk>
2021-03-14 15:42:17 +0000

    added logic for rejecting password lengths that are not numeric

--- Develop/script.js
+++ Develop/script.js
@@ -18,25 +18,34 @@ function generatePassword() {
 
 	// Password length declaration by user
 	const lengthChoice = prompt("How long would you like your password to be?");
-	// Can't be shorter than 8  or more than 128
-	if (!lengthChoice) {
-    alert("Please enter the password length you would like.");
+	// Can't be shorter than 8 or more than 128
+	if ( !lengthChoice || isNaN(lengthChoice) ) {
+    alert("Please enter the password length you would like using numerical values only.");
 	} else if ( lengthChoice > 128 || lengthChoice < 8) { 
 		alert("Please enter a password length that's more than 8 and less than 128 characters.");
 	} else {
 	// Criteria selection by user
 	// Uppercase letters
 	const uppercaseChoice = confirm("Would you like to include uppercase letters in your password?");
+	console.log (uppercaseChoice);
 	// Lowercase letters
 	const lowercaseChoice = confirm("Would you like to include lowercase letters in your password?");
 	// Numerics
 	const numericChoice = confirm("Would you like to include numbers in your password?");
+	console.log (numericChoice)
 	// Special characters
 	const specialChoice = confirm("Would you like to include any special characters in your password?");
-	// Has user selected at least one of the above?
 	}
 
-	// Has user selected at least one of the above?
+	// User criteria selection validation
+	// if doesn't work
+	if ((!uppercaseChoice) && (!lowercaseChoice) && (!numericChoice) && (!specialChoice)) {
+		alert("Please start again and select at least one password criteria to generate a password.");
+	} else {
+		// generate password
+	}
+
+
 
 
 
