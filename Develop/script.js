5f9bf17a3b1ed84879fb1d73527fc122649727be
Dominika <domi_nika@hotmail.co.uk>
2021-03-15 10:21:55 +0000

    added generate code and concate functions but does not work

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,5 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Declare a new function on line 3
 function generatePassword() {
 	// Function variables
 	var password = '';
@@ -9,70 +7,76 @@ function generatePassword() {
 	const lowercaseArray = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
 	const numericArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,];
 	const specialArray =  ["@", "%", "+", "\\", "/", "'", "!", "#", "$", "^", "?", ":", ",", ")", "(", "}", "{", "]", "[", "~", "-", "_", ".",];
+	let uppercaseChoice = false;
+	let lowercaseChoice = false;
+	let numericChoice = false;
+	let specialChoice = false;
+	let concatArray = [];
 
+	// Password length declared by user
+	const lengthChoice = prompt("How long would you like your password to be?");
 
-	declareLength ();
+	// Password length input is validated
+	validateLengthChoice ();
 
-	function declareLength () {
-		// Password length declaration by user
-		const lengthChoice = prompt("How long would you like your password to be?");
-		// Can't be shorter than 8 or more than 128
+	function validateLengthChoice () {
+		// Password cannot be shorter than 8 or more than 128 characters. Th password must be a number.
 		if ( !lengthChoice || isNaN(lengthChoice) ) {
     	alert("Please enter the password length you would like using numerical values only.");
 		} else if ( lengthChoice > 128 || lengthChoice < 8) { 
 			alert("Please enter a password length that's more than 8 and less than 128 characters.");
-		} else { criteriaChoice ();
+		} else { criteriaChoice ()
 		}
 	}
 
 	function criteriaChoice () {
-		// Criteria selection by user
-		// Uppercase letters
+		// Password criteria selection by user
 		const uppercaseChoice = confirm("Would you like to include uppercase letters in your password?");
-		console.log(uppercaseChoice)
-		// Lowercase letters
 		const lowercaseChoice = confirm("Would you like to include lowercase letters in your password?");
-		// Numerics
 		const numericChoice = confirm("Would you like to include numbers in your password?");
-		// Special characters
 		const specialChoice = confirm("Would you like to include any special characters in your password?");
+		
+		// Password criteria is validated
+		criteriaValidation (uppercaseChoice,lowercaseChoice,numericChoice,specialChoice)
+	}
+
+	function criteriaValidation (uppercaseChoice,lowercaseChoice,numericChoice,specialChoice) {
 		if (!uppercaseChoice && !lowercaseChoice && !numericChoice && !specialChoice) {
 			alert("Please start again and select at least one password criteria to generate a password.");
-		} else { concateArrays ();
+		} else { generate (uppercaseChoice,lowercaseChoice,numericChoice,specialChoice);
 		}	
 	}
 
-	// Concate chosen criteria arrays into one long array 
-	// This does not work
-	function concateArrays () {
-		const concArray = function () {
+	// New password is generated
+	function generate (uppercaseChoice,lowercaseChoice,numericChoice,specialChoice) {
+		// Required criteria arrays are joined into a single array
+		const concArray = function concArrays () { 
 			if (uppercaseChoice) {
-				concArray = concatenatedArray.concat(lowerCaseArray);
+				concArray = concArray.concat(upperCaseArray);
 			}
-			console.log (concateArrays)
-		
+
 			if (lowercaseChoice) {
-				concArray = concatenatedArray.concat(upperCaseArray);
+				concArray = concArray.concat(lowercaseArray);
 			}
-		
+			
 			if (numericChoice) {
-				concArray = concatenatedArray.concat(numberArray);
+				concArray = concArray.concat(numericArray);
 			}
-		
+			
 			if (specialChoice) {
-				concArray = concatenatedArray.concat(specialCharacterArray);
+				concArray = concArray.concat(specialArray);
 			}
 		}
+
+		console.log (concArray)
+
+		// A for loop is used to generate a password from the single array
+		for (var i = 0; i < lengthChoice; i++) {
+			let password = concArray[Math.floor(Math.random() * concArray.length)];
+		}
 	}
-
-	// for (var i = 0; i < lengthChoice; i++) {
-		// let generate = [Math.floor(Math.random() * 62)];
-		// password += generate;
-	//}
-
-	// Return our created password
-	//return password;
 }
+
 
 // Write password to the #password input
 function writePassword() {
